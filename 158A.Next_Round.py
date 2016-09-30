6612ab9c3860b15521320b681d376d31d1f314de
Wenkai Shi <dearwkshi@gmail.com>
2016-09-30 17:56:27 +0800

    AC 158A and begin 118A

--- 158A.Next_Round.py
+++ 158A.Next_Round.py
@@ -5,12 +5,16 @@ k = nk[1]
 a = [int(i) for i in input().split()]
 i = 0
 flag = 0
-if a[i] <= k:
-    print(i)
-    exit()
-while flag == 0:
-    if a[i] <= k:
+while flag == 0 and a[i] != 0:
+    if i == k-1:
         flag = 1
         continue
     i += 1
-print(i)
+if flag == 1:
+    v = a[i]
+    while i < n and a[i] == v:
+        i += 1
+if i == 0 and a[i] == 0:
+    print(0)
+else:
+    print(i)
